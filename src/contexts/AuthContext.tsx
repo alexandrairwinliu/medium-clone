fda5eca057c536008caa3172a10d5a5aaeea6e94
Alexandra Irwin Liu <45724057+alexandrairwinliu@users.noreply.github.com>
2022-12-30 11:11:16 +1100

    Fixed up my shitty merge conflict mistakes

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -4,23 +4,26 @@ import { createContext, ReactNode, useState } from "react";
 // Oh wait actually there's more to it than that
 // https://medium.com/@martin_hotell/interface-vs-type-alias-in-typescript-2-7-2a8f1777af4c
 
+// AuthContext
 type authContextType = {
   user: boolean;
   login: () => void;
   logout: () => void;
 };
 
-// AuthContext
 const authContextDefaultValues: authContextType = {
   user: false,
   login: () => {},
   logout: () => {},
 };
 
+export const AuthContext = createContext<authContextType>(authContextDefaultValues);
+
+// AuthProvider function
 type authProviderProps = {
   children: ReactNode;
 };
-// AuthProvider function
+
 export const AuthProvider = ({ children }: authProviderProps) => {
   const [user, setUser] = useState<boolean>(null);
   const login = () => {
@@ -34,27 +37,6 @@ export const AuthProvider = ({ children }: authProviderProps) => {
   return <AuthContext.Provider value={(user, login, logout)}>{children}</AuthContext.Provider>;
 };
 
-// AuthContext
-const authContextDefaultValues: authContextType = {
-  user: false,
-  login: () => {},
-  logout: () => {},
-};
-
-export const AuthContext = createContext<authContextType>(authContextDefaultValues);
-
 type authProviderProps = {
   children: ReactNode;
-};
-// AuthProvider function
-export const AuthProvider = ({ children }: authProviderProps) => {
-  const [user, setUser] = useState<boolean>(false);
-  const login = () => {
-    setUser(true);
-  };
-  const logout = () => {
-    setUser(false);
-  };
-
-  return <AuthContext.Provider value={(user, login, logout)}>{children}</AuthContext.Provider>;
 };
